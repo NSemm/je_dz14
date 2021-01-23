8501be55f27275efe72b3d995a5c4b2692c25fae
NickSemerenko <nickolay.sem@gmail.com>
2021-01-23 19:18:03 +0200

    all files to list

--- src/main/java/com/k7/Main.java
+++ src/main/java/com/k7/Main.java
@@ -1,22 +1,31 @@
 package com.k7;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
 
-    public static File findAbsoluteFile(File dir, String fileName) {
-        File rez = null;
+    public static List<File> findAbsoluteFile(List<File> rezList, File dir, String fileName) {
         for (File list : dir.listFiles()) {
-            if (list.isDirectory()) findAbsoluteFile(list.getAbsoluteFile(), fileName);
-            rez = list.getName().equals(fileName) ? list.getAbsoluteFile() : null;
+            if (list.isDirectory()) findAbsoluteFile(rezList, list.getAbsoluteFile(), fileName);
+            if (list.getName().equals(fileName)) rezList.add(new File(list.getAbsoluteFile().getPath()));
         }
-        System.out.println(rez);
-        return rez;
+        //System.out.println(rez);
+        return rezList;
     }
 
     public static void main(String[] args) {
         File file = new File("test.txt");
         File path = new File(".");
-        findAbsoluteFile(path, "test.txt").listFiles();
+        List<File> rez = new LinkedList<>();
+        if (path.isDirectory() && path.exists()) {
+            findAbsoluteFile(rez, path, "test.txt");
+            for (File f : rez) {
+                System.out.println(f);
+            }
+        } else {
+            System.out.println("Invalid path or not directory");
+        }
     }
 }
